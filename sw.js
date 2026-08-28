7adffbde966cdc7ceabe5e3e93eb8d45c2d9d5dc
ybagaini-sys <ybagaini@gmail.com>
2026-08-28 10:07:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-const CACHE='uscm-temps-jeu-v1';
+const CACHE='uscm-temps-jeu-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./sw.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-  const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
-}).catch(()=>caches.match('./index.html')))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>caches.match('./index.html')))));
