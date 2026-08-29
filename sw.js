53a2b115753ac53531653fecf1ff5eecd554773b
ybagaini-sys <ybagaini@gmail.com>
2026-08-29 16:45:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,33 @@
-const CACHE='uscm-temps-jeu-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./sw.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return res;}).catch(()=>caches.match('./index.html')))));
+const CACHE = 'uscm-temps-de-jeu-v31';
+const APP = './index.html';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll([APP, './manifest.webmanifest', './icon-192.png', './icon-512.png']))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(req)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(req, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(req).then(cached => cached || caches.match(APP)))
+  );
+});
