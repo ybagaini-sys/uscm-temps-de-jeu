abb81aec8e165dc37ba91d6007ea52157bf58137
ybagaini-sys <ybagaini@gmail.com>
2026-08-30 00:22:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,8 @@
-const CACHE = 'uscm-temps-de-jeu-v31';
-const APP = './index.html';
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll([APP, './manifest.webmanifest', './icon-192.png', './icon-512.png']))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  if (req.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(req)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(req, copy)).catch(() => {});
-        return response;
-      })
-      .catch(() => caches.match(req).then(cached => cached || caches.match(APP)))
-  );
+const CACHE='uscm-temps-de-jeu-v31-persist';
+const APP='./index.html';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll([APP,'./manifest.webmanifest'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET') return;
+ e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match(APP))));
 });
