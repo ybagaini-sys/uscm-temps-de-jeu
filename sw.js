e256df04dd2635e64185c10813807d22c8796524
ybagaini-sys <ybagaini@gmail.com>
2026-09-18 16:52:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='uscm-temps-de-jeu-v35-6-contingent-order';
+const CACHE='uscm-temps-de-jeu-v36-1-audit';
 const APP='./index.html';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll([APP,'./manifest.webmanifest'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
