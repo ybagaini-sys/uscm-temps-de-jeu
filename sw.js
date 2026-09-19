7bfd9a246032eecace39918bd49eb96649dd47f0
ybagaini-sys <ybagaini@gmail.com>
2026-09-19 09:13:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,13 @@
-const CACHE='uscm-temps-de-jeu-v36-3-time-sync';
-const APP='./index.html';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll([APP,'./manifest.webmanifest'])).then(()=>self.skipWaiting())));
+const CACHE='uscm-temps-de-jeu-v36-7-fiable';
+const CORE=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET') return;
- e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match(APP))));
+  if(e.request.method!=='GET') return;
+  const url=new URL(e.request.url);
+  if(url.origin===location.origin && (url.pathname.endsWith('/index.html') || url.pathname.endsWith('/'))){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c)).catch(()=>{});return r}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
 });
